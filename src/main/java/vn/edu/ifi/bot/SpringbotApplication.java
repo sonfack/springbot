38c4367cbab97153fa8af7f9e556746f57ccc93a
SONFACK SOUNCHIO Serge <sss.sonfack@gmail.com>
2018-06-07 02:47:40 +0700

    Initialisation du projet de chatbot utilisant springboot

---
+++ src/main/java/vn/edu/ifi/bot/SpringbotApplication.java
@@ -0,0 +1,12 @@
+package vn.edu.ifi.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbotApplication.class, args);
+	}
+}
